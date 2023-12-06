bbb521fec30b83cdcb68adbdf723c41e3f503a1f
Diego Henrique Diniz <115659288+Diego-Diniz@users.noreply.github.com>
2023-12-06 20:00:08 -0300

    learning

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = "Diego";
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente("Diego", 61695491904);
+const cliente2 = new Cliente("Alice", 88122244309);
 
 
-const cliente2 = new Cliente();
- cliente2.nome = "Alice";
- cliente2.cpf = 88122244309;
 
- const contaCorrenteDiego = new ContaCorrente();
- contaCorrenteDiego.agencia = 1001;
- contaCorrenteDiego.cliente = cliente1;
- contaCorrenteDiego.depositar(500);
+const contaCorrenteDiego = new ContaCorrente(1001, cliente1);
 
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 102;
+
+contaCorrenteDiego.depositar(500);
+
+
+
 
 let valor = 200;
 
-contaCorrenteDiego.tranferir(valor,conta2);
+contaCorrenteDiego.tranferir(valor, conta2);
 
-console.log("valor: ", valor);
-console.log(conta2);
-console.log(contaCorrenteDiego);
+console.log(ContaCorrente.numeroDeContas);
 
 
 
